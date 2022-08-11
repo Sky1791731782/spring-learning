58406ad64eb60ee30d45d0227948fa359536ebea
sky1791731782 <1791731782@qq.com>
2022-08-11 16:06:28 +0800

    @Resource

---
+++ part3/src/main/java/sky/demo5/Student.java
@@ -0,0 +1,50 @@
+package sky.demo5;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.ImportResource;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.Resource;
+
+/*
+    引用类型:
+    @Resource:来自jdk中的注解，spring框架提供了对这个注解的功能支持，可以用来给引用类型赋值
+    使用的也是自动注入的方式，支持byName，byType默认是byName
+    位置:
+            1.属性定义的上面无需set方法，推荐使用
+            2.在set方法上面
+     先使用byName，如果不行则使用byType
+*/
+
+@Component(value = "mystudent")
+public class Student {
+    @Value("鞠婧祎")
+    private String name;
+    @Value("20")
+    private int age;
+    @Resource
+    private School school;
+    public void setSchool(School school) {
+        this.school = school;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                ", school=" + school +
+                '}';
+    }
+
+    public void setName(String name) {
+
+        this.name = name;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
---
+++ part3/src/test/java/sky/Mytest5.java
@@ -0,0 +1,16 @@
+package sky;
+
+import org.junit.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import sky.demo5.Student;
+
+public class Mytest5 {
+    @Test
+    public void test01(){
+        String config = "memo3/applicationContext.xml";
+        ApplicationContext ac = new ClassPathXmlApplicationContext(config);
+        Student student = (Student) ac.getBean("mystudent");
+        System.out.println(student);
+    }
+}
